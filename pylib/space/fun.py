069ccedd38dbb176f3b3ce7d11026ba821f48f17
shawnafk <zhengjs@mail.ustc.edu.cn>
2024-11-04 13:24:38 +0800

    merged fun but remove them after

--- pylib/space/fun.py
+++ pylib/space/fun.py
@@ -1,19 +1,150 @@
 import numpy as np
 import matplotlib.pyplot  as plt
 from datetime import datetime, timedelta
+from astropy.time import Time
+import pytz
 import pyspedas
 from matplotlib.collections import LineCollection
 from matplotlib.colors import ListedColormap, BoundaryNorm
 import matplotlib.dates as mdates
 
-
 RE = 6371.393
-def plot_earth(ax):
-    t = np.linspace(0,2*np.pi,128)
-    x = np.cos(t)
-    y = np.sin(t)
-    ax.plot(x,y,'k')
-    return 0
+# ---------------------------- rotation ------------------------------
+def q2rot(q1,q2,q3,N):
+    mat = np.zeros((N,3,3))
+    q0 = np.sqrt(1-q1**2-q2**2-q3**2)
+    mat[:,0,0] = 1 - 2*q2**2 - 2* q3**2
+    mat[:,0,1] = 2*q1*q2 - 2* q0 * q3
+    mat[:,0,2] = 2 * q1 * q3 + 2 * q0 * q2
+         
+    mat[:,1,0] = 2*q1*q2 + 2* q0 * q3
+    mat[:,1,1] = 1 - 2*q1**2 - 2* q3**2
+    mat[:,1,2] = 2 * q2 * q3 - 2 * q0 * q1
+         
+    mat[:,2,0] = 2*q1*q3 - 2* q0 * q2
+    mat[:,2,1] = 2*q2*q3 + 2* q0 * q1
+    mat[:,2,2] = 1 - 2*q1**2 - 2*q2**2
+    return mat
+
+#only one
+def rot_x(deg):
+    r11 = 1
+    r12 = 0
+    r13 = 0
+    
+    r21 = 0
+    r22 = np.cos(deg)
+    r23 = -np.sin(deg)
+    
+    r31 = 0
+    r32 = np.sin(deg)
+    r33 = np.cos(deg)
+    
+    mat = np.array([[r11,r12,r13],[r21,r22,r23],[r31,r32,r33]])
+    return mat
+
+def rot_y(deg):
+    r11 = np.cos(deg)
+    r12 = 0
+    r13 = np.sin(deg)
+    
+    r21 = 0
+    r22 = 1
+    r23 = 0
+    
+    r31 = -np.sin(deg)
+    r32 = 0
+    r33 = np.cos(deg)
+    
+    mat = np.array([[r11,r12,r13],[r21,r22,r23],[r31,r32,r33]])
+    return mat
+
+def rot_z(deg):
+    r11 = np.cos(deg)
+    r12 = -np.sin(deg)
+    r13 = 0
+    
+    r21 = np.sin(deg)
+    r22 = np.cos(deg)
+    r23 = 0
+    
+    r31 = 0
+    r32 = 0
+    r33 = 1
+    
+    mat = np.array([[r11,r12,r13],[r21,r22,r23],[r31,r32,r33]])
+    return mat
+
+# ---------------------------- time manipulation ------------------------------
+
+def strtime2timestamp(time_str,format = '%Y-%m-%d %H:%M:%S'):
+    N  = time_str.shape[0]
+    dt = np.zeros(N)
+    for i in range(N):
+        dt[i]=datetime.strptime(time_str[i][:19], format).timestamp()
+    return dt
+
+def timestamp2isot(ts):
+    N  = ts.shape[0]
+    isot=np.zeros(N,dtype='object')
+    for i in range(N):
+        dt_object = datetime.fromtimestamp(ts[i], pytz.timezone('UTC'))
+        #iso_format_string = dt_object.isoformat()
+        isot[i] = Time(dt_object, scale='utc')
+    return isot
+
+#to juliandate
+def isot2jd(isot):
+    N  = isot.shape[0]
+    t_jd=np.zeros(N)
+    for i in range(N):
+        t_jd[i] = isot[i].jd   
+    return t_jd
+
+def str_to_timestamp(time_str):
+    # 将字符串按照'年月日时分秒'的格式解析为datetime对象
+    dt = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')
+    # 将datetime对象转换为Unix时间戳（单位为秒）
+    timestamp = int(dt.timestamp())
+    return timestamp
+
+def timestamp_to_str(timestamp):
+    # 将Unix时间戳转换为datetime对象
+    dt = datetime.fromtimestamp(timestamp)
+    # 将datetime对象格式化为'年月日时分秒'的字符串
+    time_str = dt.strftime('%Y-%m-%d %H:%M:%S')
+    return time_str
+
+def find_closest_index(timestamp_list, t0):
+    left, right = 0, len(timestamp_list) - 1
+    closest_index = -1
+    min_diff = float('inf')  # 正无穷大
+
+    while left <= right:
+        mid = (left + right) // 2
+        current_diff = abs(timestamp_list[mid] - t0)
+
+        # 如果当前的差值小于之前的最小差值，则更新最接近的索引和最小差值
+        if current_diff < min_diff:
+            min_diff = current_diff
+            closest_index = mid
+
+        # 如果当前时间戳小于t0，则在右半部分继续查找
+        if timestamp_list[mid] < t0:
+            left = mid + 1
+        # 如果当前时间戳大于t0，则在左半部分继续查找
+        else:
+            right = mid - 1
+
+    return closest_index
+
+def get_idx_of_times(t0,dt,n,ts):
+    t0 = str_to_timestamp(t0)
+    target_time = t0 + np.arange(0,n)*dt
+    idx = []
+    for i in range(n):
+        idx.append(find_closest_index(ts,target_time[i]))
+    return idx
 
 def convert_to_datetime(year, day_of_year, hour_fraction):
     start_date = datetime(year, 1, 1)
@@ -207,6 +338,12 @@ def rot_dec(dec):
 #            counts[i+(nbins)*r] = data[r,bin_s+i]
 #    return coords,counts
 
+def load_omni(t,str):
+    date1 = t[0].strftime('%Y-%m-%d-%H-%M')
+    date2 = t[-1].strftime('%Y-%m-%d-%H-%M')
+    omni_vars = pyspedas.omni.data(trange=[date1, date2],notplot=True,varnames = [str])
+    return omni_vars
+#get X-R(Z/Y, assuming axisymmectric) location of MP and BS
 def loc_bs_mp(t):
     date1 = t[0].strftime('%Y-%m-%d-%H-%M')
     date2 = t[-1].strftime('%Y-%m-%d-%H-%M')
@@ -310,4 +447,53 @@ def xz_counts(orbit,pointing,counts,flag):
             #value
             recounts[i+(nbins)*r] = counts[r,bin_start+i]
     return coords,recounts
+
+import os
+from space import fun
+import PyGeopack as gp
+def plot_fieldline(time,ax,c):
+    f = 'BF'+time+'.npy'
+    if os.path.exists(f):
+        Tall = np.load(f,allow_pickle=True)
+        for i in range(30):
+            for j in range(20):
+                ax.plot(Tall[j].xgse[i,:],Tall[j].zgse[i,:],c,lw=0.15)
+    else:
+        Tall = []
+        for i in np.arange(-10,10):
+            #x, y, z
+            Tall.append(gp.TraceField(np.linspace(-20,20,40),0,i,int(time),2))
+        for i in range(30):
+            for j in range(20):
+                ax.plot(Tall[j].xgse[i,:],Tall[j].zgse[i,:],c,lw=0.15)
+        np.save(f,Tall)
+    fun.plot_earth(ax)
+    return 0
+
+def show_FOV(ax,vertex):
+    x=[]
+    y=[]
+    for p in vertex:
+        x.append(p[0])
+        y.append(p[1])
+        ax.scatter(p[0],p[1],marker='+',color='k')
+    x.append(vertex[0][0])
+    y.append(vertex[0][1])
+    ax.plot(x,y)
+    return 0
+
+def plot_earth(ax):
+    t = np.linspace(0,2*np.pi,128)
+    x = np.cos(t)
+    y = np.sin(t)
+    ax.plot(x,y,'k')
+    return 0
+
+
+
+def nearest_value(t0,epoch,value):
+    epoch2ts = [dt.timestamp() for dt in epoch]
+    idx = find_closest_index(epoch2ts,t0)
+    return value[idx]
+    
 
